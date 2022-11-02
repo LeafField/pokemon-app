fe7bd1630d4c02047e93eab232841bd5744850cc
LeafField <109466326+LeafField@users.noreply.github.com>
2022-11-02 21:50:22 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import Card from "./components/card/Card";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
+
+function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fecthPokemonData = async () => {
+      // すべてのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      // 各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      console.log(res.results);
+      setLoading(false);
+    };
+    fecthPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
